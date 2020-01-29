a0714e3f23ef99f82032eb9f0c355eaab49598b4
Molonlabee <janayahernandez@yahoo.com>
2020-01-28 23:49:17 -0600

    react-drills

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Image from "./image";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Image url={"https://i.pinimg.com/originals/ac/3c/52/ac3c524a774ebf943d2be23e77a02ad7.jpg"} />
+      </div>
+    );
+  }
 }
 
 export default App;
